b9d90ed2c915193e0052e038acd05effdc6f4a54
kuba <kuba456@gmail.com>
2018-12-01 23:02:42 -0800

    first example from the MVC documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,30 @@
 #include <QApplication>
-
-#include "mainwindow.h"
+#include <QtGui>
+#include <QSplitter>
+#include <QFileSystemModel>
+#include <QTreeView>
+#include <QListView>
 
 int main(int argc, char *argv[])
 {
     Q_INIT_RESOURCE(simpletreemodel);
 
     QApplication app(argc, argv);
-    MainWindow window;
-    window.show();
+    QSplitter *splitter = new QSplitter;
+    QFileSystemModel *model = new QFileSystemModel;
+    model->setRootPath(QDir::currentPath());
+    
+    /// create two views:
+    QTreeView *tree = new QTreeView(splitter);
+    tree->setModel(model);
+    tree->setRootIndex(model->index(QDir::currentPath()));
+    
+    QListView *list = new QListView(splitter);
+    list->setModel(model);
+    list->setRootIndex(model->index(QDir::currentPath()));
+    
+    splitter->setWindowTitle("Two views onto the same file system model");
+    splitter->show();
+    
     return app.exec();
 }
